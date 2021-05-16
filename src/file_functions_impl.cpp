faa8316b146d04dde7e22a4e3cce46e72d0f09a8
ViniciusAriel <viniciusarielarruda@tutanota.com>
2021-05-15 21:23:10 -0300

    Refactoring of GetImageSize function

--- src/file_functions_impl.cpp
+++ src/file_functions_impl.cpp
@@ -14,38 +14,28 @@ std::streamsize GetFileSize(std::fstream& file){
   return file_size;
 }
 
+std::string ReadWordFromData(std::vector<char>& file_data){
+  std::string word;
+  for(int i{0}; i < file_data.size(); ++i){
+    word.push_back(file_data[i]);
+
+    if(iswspace(file_data[i])){
+      file_data.erase(file_data.begin(), file_data.begin() + i + 1);
+      return word;
+    }
+  }
+}
+
 ImageSize GetImageSize(std::vector<char>& file_data){
   bool magic_number_read{false};
   std::string number_string;
 
   ImageSize image_size;
 
-  for(int i{0}; i < file_data.size(); ++i){
-    //creates string from buffer data
-    number_string.push_back(file_data[i]);
-
-    //Read magic number
-    if(iswspace(file_data[i]) && !magic_number_read){
-      magic_number_read = true;
-      number_string = "";
-    }
-    //Reads width
-    else if(iswspace(file_data[i]) && image_size.width == 0){
-      image_size.width = std::atoi(number_string.c_str());
-      number_string = "";
-    }
-    //Reads height
-    else if(iswspace(file_data[i]) && image_size.height == 0){
-      image_size.height = std::atoi(number_string.c_str());
-      number_string = "";
-    }
-    //Ignores the highest value for each pixel
-    else if(iswspace(file_data[i])){
-      //Erases read data from buffer so we don't have to store where we left off
-      file_data.erase(file_data.begin(), file_data.begin() + i + 1);
-      break;
-    }
-  }
+  int magic_number{std::atoi(ReadWordFromData(file_data).c_str())};
+  image_size.width = std::atoi(ReadWordFromData(file_data).c_str());
+  image_size.height = std::atoi(ReadWordFromData(file_data).c_str());
+  int max_greyscale_value{std::atoi(ReadWordFromData(file_data).c_str())};
 
   return image_size;
 }
